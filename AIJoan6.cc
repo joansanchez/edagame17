67827adff60a57b3501802223fb3ff8f0b78b2f1
Joan Sanchez <jsanchezgarcia13@gmail.com>
2017-05-10 09:38:42 +0200

    first functional version accepted by the judge

--- AIJoan6.cc
+++ AIJoan6.cc
@@ -8,7 +8,7 @@ using namespace std;
  * Write the name of your player and save this file
  * with a name matching the format "AIyourname.cc".
  */
-#define PLAYER_NAME Joan6
+#define PLAYER_NAME YungBeef
 
 
 struct PLAYER_NAME : public Player {
@@ -26,14 +26,26 @@ struct PLAYER_NAME : public Player {
      * Attributes for your player can be defined here.
      */
     //vector<int> my_awesome_vector_of_integers;
+    void omplirvert (vector<int>& dang){
+      for (int i = 0; i < (nb_bikes() * nb_players()-1); ++ i){
+        if(bike(i).player != me()) dang[i] = bike(i).id;
+      }
+    }
 
-    int dfs_recursi(int id, vector <bool>& vis, int dist){
-      if (not vis[id] and vertex(id).wall == -1 and vertex(id).bike == -1){
+    bool cleanvertice(int id, const vector<int>& dang){
+      for (int i = 0; i < (int)dang.size(); ++i){
+        if (is_neighbour(id, dang[i])) return false;
+      }
+      return true;
+    }
+
+    int dfs_recursi(int id, vector <bool>& vis, int dist, const vector<int>& dang){
+      if (not vis[id] and vertex(id).wall == -1 and vertex(id).bike == -1 and cleanvertice(id, dang)){
         int max = dist;
         vis[id] = true;
         vector<int> veins = vertex(id).neighbours;
         for(int v : veins){
-          int aux = dfs_recursi(v, vis, dist + 1);
+          int aux = dfs_recursi(v, vis, dist + 1, dang);
           if (aux > max) max = aux;
         }
         return max;
@@ -42,66 +54,56 @@ struct PLAYER_NAME : public Player {
     }
 
 
-    int dfs_rec(int id){
+    int dfs_rec(int id, const set<int>& nextmove, const vector<int>& dang){
       int max = 0;
       int dir = -1;
       vector<int> neighbours = vertex(id).neighbours;
-      for (int u = 0; u < neighbours.size(); ++ u){
+      for (int u = 0; u < (int)neighbours.size(); ++ u){
         vector <bool> vis (nb_vertices(), false);
         vis[id] = true;
-        int aux = dfs_recursi(neighbours[u], vis, 0);
-        if (aux > max){
-          max = aux;
-          dir = neighbours[u];
+        set<int>::iterator it;
+        it = nextmove.find(neighbours[u]);
+        if(it == nextmove.end()){
+          int aux = dfs_recursi(neighbours[u], vis, 0, dang);
+          if (aux > max){
+            max = aux;
+            dir = neighbours[u];
+          }
         }
       }
       return dir;
     }
 
     void play () {
-
+        set<int> nextmove;
         vector<int> my_bikes = bikes(me());
         for (int i = 0; i < (int)my_bikes.size(); ++i) {
-  			const Bike& my_bike = bike(my_bikes[i]);
+    			const Bike& my_bike = bike(my_bikes[i]);
+          vector<int> dang((nb_bikes() * nb_players())-1);
+          omplirvert (dang);
+    			// Do something only if this bike is alive
+    			if (!my_bike.alive) {
+    				continue;
+    			}
 
-  			// Do something only if this bike is alive
-  			if (!my_bike.alive) {
-  				continue;
-  			}
+    			//Only turbo bikes can move at odd rounds
+    			if (round() % 2 && my_bike.turbo_duration <= 0) {
+    				continue;
+    			}
 
-  			//Only turbo bikes can move at odd rounds
-  			if (round() % 2 && my_bike.turbo_duration <= 0) {
-  				continue;
-  			}
+          int mo = dfs_rec(my_bike.vertex, nextmove, dang);
 
-        int mo = dfs_rec(my_bike.vertex);
-  			// Find all empty neighbours
-  			/*vector<int> neighbours = vertex(my_bike.vertex).neighbours;
-  			vector<int> empty_neighbours;
-  			for (int i = 0; i < (int)neighbours.size(); i++) {
-  				int id = neighbours[i];
-  				if (vertex(id).wall == -1) {
-  					empty_neighbours.push_back(id);
-  				}
-  			}*/
 
-  			// Create an empty movement
-  			Movement movement(my_bike.id);
-
-  			// Set next_vertex to a random empty neighbour, if any, or to a random neighbour otherwise
-  		/*	if (!empty_neighbours.empty()) {
-  				movement.next_vertex = empty_neighbours[ rand() % (int)empty_neighbours.size() ];
-  			} else {
-  				movement.next_vertex = neighbours[ rand() % (int)neighbours.size() ];
-  			}*/
-        movement.next_vertex = mo;
-  			// Use bonus randomly when we have one
-  			if (my_bike.bonus != None && rand()%5 > 3) {
-  				movement.use_bonus = true;
-  			}
-
-  			// Command the movement
-            command(movement);
+    			// Create an empty movement
+    			Movement movement(my_bike.id);
+          movement.next_vertex = mo;
+    			// Use bonus randomly when we have one
+    			if (my_bike.bonus != None && rand()%5 > 3) {
+    				movement.use_bonus = true;
+    			}
+          nextmove.insert(mo);
+    			// Command the movement
+              command(movement);
 
         }
 
